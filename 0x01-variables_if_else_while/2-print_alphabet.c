00421c210bae4630f1a6b62b74dae0d91e5ac46a
AdexJay <adepojujn@gmail.com>
2022-09-09 16:14:14 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes in here */
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always return an integer
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++) putchar(a);
+	putchar('\n');
+	return (0);
+}
