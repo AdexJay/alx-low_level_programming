df32ab5343f2f91c4522b0aaee951d30865adac3
AdexJay <adepojujn@gmail.com>
2022-09-22 11:43:23 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,15 +10,17 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, int j;
+	int c, c2;
 
-	i = j = 0;
-	while (*(frdy + i))
-		i++;
-	while ((*(dest + i) = *(src + j)))
-	{
-		i++;
-		j++;
-	}
+	c = 0;
+	/*find the size of dest array*/
+	while (dest[c])
+		c++;
+
+	/* iterate through each src array value without the null byte*/
+	for (c2 = 0; src[c2] ; c2++)
+		/*append src[c2] to dest[c] while overwritting the null byte in dest*/
+		dest[c++] = src[c2];
+
 	return (dest);
 }
