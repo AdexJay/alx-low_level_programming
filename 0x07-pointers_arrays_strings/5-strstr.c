109f08b4ffc4f63581f8a25b9a1ef6ae7e8644df
AdexJay <adepojujn@gmail.com>
2022-09-26 14:25:18 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,31 +2,29 @@
 
 /**
  * _strstr - locates a substring
- * @haystack: string in which to check for needle
- * @needle: substring to find in haystack
  *
- * Return: pointer to beginning of needle in haystack or NULL if no match
+ * @needle: occurance to find
+ *
+ * @haystack: string to search
+ *
+ * Return: pointer
  */
+
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int i = 0, j = 0;
-
-	while (haystack[i])
+	while (*haystack)
 	{
-		while (needle[j] && (haystack[i] == needle[0]))
+		char *src = haystack;
+		char *sub = needle;
+
+		while (*haystack && *sub && *haystack == *sub)
 		{
-			if (haystack[i + j] == needle[j])
-				j++;
-			else
-				break;
+			haystack++;
+			sub++;
 		}
-		if (needle[j])
-		{
-			i++;
-			j = 0;
-		}
-		else
-			return (haystack + i);
+		if (!*sub)
+			return (src);
+		haystack = src + 1;
 	}
 	return (0);
 }
