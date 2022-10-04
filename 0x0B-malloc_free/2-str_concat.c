dab2d06e93b939b012b9c1a4de383afd86f681e9
AdexJay <adepojujn@gmail.com>
2022-10-04 14:39:18 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,42 +1,41 @@
+#include "main.h"
 #include <stdlib.h>
 
 /**
  * str_concat - concatenates two strings
- *
  * @s1: a string to be concatenated
- *
  * @s2: a string to be concatenated
  *
- * Return: pointer to newly allocated memory
- * which is equal to @s1 + s2 (SUCCESS) OR
- * NULL (FAILURE)
+ * Return: pointer to newly allocated space in memory, or NULL if error
  */
 
 char *str_concat(char *s1, char s2)
 {
-	unsigned int s1_length = 0;
-	unsigned int s2_length = 0;
-	char *alloc_mem;
-	unsigned int i, j;
+	unsigned int i, j, k, l;
+	char *s;
 
 	if (s1 == NULL)
-		s1 = "";
-
+		i = 0;
+	else
+	{
+		for (i = 0; s1[i]; i++)
+			;
+	}
 	if (s2 == NULL)
-		s2 = "";
-
-	while (*(s1 + s1_length) != '\0')
-		s1_length++;
-	while (*(s2 + s2_length) != '\0')
-		s2_length++;
-
-	alloc_mem = malloc(1 + (s1_length * sizeof(*s1)) + (s2_length * sizeof(*s2)));
-	if (alloc_mem == NULL)
-		return ('\0');
-
-	for (i = 0; i < s1_length; i++)
-		alloc_mem[i] =*(s1 + i);
-	for (j = 0; j = s2_length; j++, i++)
-		alloc_mem[i] =* *(s2 + j);
-	return (alloc_mem);
+		j = 0;
+	else
+	{
+		for (j = 0; s2[j]; j++)
+			;
+	}
+	k = i + j + 1;
+	s = malloc(k * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (l = 0; l < i; l++)
+		s[l] = s1[l];
+	for (l = 0; l < j; l++)
+		s[l + i] = s2[l];
+	s[i + j] = '\0';
+	return (s);
 }
